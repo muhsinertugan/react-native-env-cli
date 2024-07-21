3fed201303a1f5e2eb01dfed9b35a2988a906580
Muhsin Ertuğan <muhsin.ertugan@sisal.com>
2024-07-21 16:07:36 +0300

    Add initial entrypoint for the cli

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
 #!/usr/bin/env node
 
-const hello = () => {}
+import { Command } from 'commander';
+import { TOOL_GROUPS } from './constants/index.js';
+import { checkVersion } from './check/checkVersion.js';
+import { ToolGroups } from './types/toolTypes.js';
 
-console.log("Hwllo!")
+//declere the program
+
+const program = new Command();
+
+//Add actions to CLI
+
+program
+	.name('setup-env')
+	.description('CLI for setting up the initial env for React Native Projects')
+	.version('0.0.1')
+	.action(async () => {
+		const versions = await checkVersion();
+		console.log(versions);
+	});
+
+// Execute the CLI
+
+program.parse(process.argv);
+
+
