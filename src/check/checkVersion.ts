5c59dad4318f19b4ba0b7af5428e0be4f129dcdd
Muhsin Ertuğan <muhsin.ertugan@sisal.com>
2024-07-21 16:33:28 +0300

    Implement special version checks for Watchman and Xcode

--- src/check/checkVersion.ts
+++ src/check/checkVersion.ts
@@ -18,12 +18,34 @@ async function checkVersion() {
 					);
 
 					if (exists) {
-						const satisfies = semver.satisfies(
-							userEnv[toolGroup][key].version,
-							TOOLS[toolGroup as keyof ToolGroups][key]!.versionRange
-						);
+						/**
+						 * This check needed to be done to comply xCode version to semver.
+						 */
+						if (key === TOOLS.IDEs.Xcode?.name) {
+							const userXcodeVersion: string = `${
+								userEnv[toolGroup][key].version.split('/')[0]
+							}.0`;
 
-						(results as Record<string, boolean>)[key] = satisfies;
+							const satisfies = semver.satisfies(
+								userXcodeVersion,
+								TOOLS[toolGroup as keyof ToolGroups][key]!.versionRange
+							);
+
+							(results as Record<string, boolean>)[key] = satisfies;
+						} else if (key === TOOLS.Binaries.Watchman?.name) {
+							/**
+							 * Watchman does not produce a semver compliant versioning.
+							 * If watchman exist in any version it will pass the check.
+							 */
+							(results as Record<string, boolean>)[key] = true;
+						} else {
+							const satisfies = semver.satisfies(
+								userEnv[toolGroup][key].version,
+								TOOLS[toolGroup as keyof ToolGroups][key]!.versionRange
+							);
+
+							(results as Record<string, boolean>)[key] = satisfies;
+						}
 					}
 				} catch (error) {
 					console.log(error);
