39501d1128b375b715fcded41c3cc76444ac8e82
Muhsin Ertuğan <muhsin.ertugan@sisal.com>
2024-09-24 21:41:05 +0300

    Refactor envVariables.ts to update Android SDK and Java JDK paths

--- src/constants/envVariables.ts
+++ src/constants/envVariables.ts
@@ -1,10 +1,13 @@
 import { EnvTypes } from '../types/envTypes.js';
 
 const ENV_VARIABLES: EnvTypes = {
-	SDKs: {
-		Android_SDK: {
-			ANDROID_HOME: { path: '/Library/Android/sdk' },
-			ANDROID_SDK_ROOT: { path: '/Library/Android/sdk' },
+	Android_SDK: {
+		ANDROID_HOME: { path: '/Library/Android/sdk' },
+		ANDROID_SDK_ROOT: { path: '/Library/Android/sdk' },
+	},
+	Java_JDK: {
+		JAVA_HOME: {
+			path: '/path/to/java/home',
 		},
 	},
 };
